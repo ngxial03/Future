e70430e617ef0f7f04b87ba07cec694578a6164c
U-Edward_Wu-PC\Edward_Wu <Edward_Wu@Edward_Wu-PC.(none)>
2019-07-14 23:56:33 +0800

    update

--- alg.py
+++ alg.py
@@ -1,9 +1,10 @@
 import function
 import function2
 
-#TX5_DIR = "C:/cygwin64/home/Edward_Wu/SourceCodes/Future/tx5_test"
-TX5_DIR = "/Users/edward_cc_wu/SourceCode/Future/tx5_test"
-TX_DAILY_DIR = "/Users/edward_cc_wu/SourceCode/Future/daily.txt"
+TX5_DIR = "C:/cygwin64/home/Edward_Wu/SourceCodes/Future/tx5_test"
+#TX5_DIR = "/Users/edward_cc_wu/SourceCode/Future/tx5_test"
+TX_DAILY_DIR = "C:/cygwin64/home/Edward_Wu/SourceCodes/Future/daily.txt"
+#TX_DAILY_DIR = "/Users/edward_cc_wu/SourceCode/Future/daily.txt"
 BASE_RANGE = 3
 PRE_BREAK_INDEX = 2
 PRE_BREAK_AMPLITUDE = 5
--- daily_gen.py
+++ daily_gen.py
@@ -1,9 +1,10 @@
 import os
 import function
 
-#TX5_DIR = "C:/cygwin64/home/Edward_Wu/SourceCodes/Future/tx5_test"
-TX5_DIR = "/Users/edward_cc_wu/SourceCode/Future/tx5_test"
-TX_DAILY_DIR = "/Users/edward_cc_wu/SourceCode/Future/daily.txt"
+TX5_DIR = "C:/cygwin64/home/Edward_Wu/SourceCodes/Future/tx5_test"
+#TX5_DIR = "/Users/edward_cc_wu/SourceCode/Future/tx5_test"
+TX_DAILY_DIR = "C:/cygwin64/home/Edward_Wu/SourceCodes/Future/daily.txt"
+#TX_DAILY_DIR = "/Users/edward_cc_wu/SourceCode/Future/daily.txt"
 
 
 def go():
