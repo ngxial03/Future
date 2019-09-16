3a7f4539ba73e1b2035d8ac0ce7a543871141735
Edward CC Wu <ngxial03@outlook.com>
2019-09-16 14:11:01 +0800

    fix enter time when pre enter occor

--- policy/key_point.py
+++ policy/key_point.py
@@ -10,11 +10,7 @@ def get_key_point(break_point, pre_en_point, return_scale):
         direction = break_point['direction']
         index = break_point['index']
 
-    if (pre_en_point['index'] != -1) & (pre_en_point['index'] < break_point['index']):
-        # key_point = pre_en_point['pre_enter_point']
-        # direction = pre_en_point['direction']
-        # index = pre_en_point['index']
-        # is_pre_enter = True
+    if (pre_en_point['index'] != -1) & (break_point['index'] != -1) & (pre_en_point['index'] < break_point['index']):
         if break_point['index'] - pre_en_point['index'] <= 1:
             key_point = pre_en_point['pre_enter_point']
             direction = pre_en_point['direction']
@@ -26,12 +22,6 @@ def get_key_point(break_point, pre_en_point, return_scale):
                                                                                                       'min'] + return_value
             direction = break_point['direction']
             index = break_point['index']
-        #     return_value = 1
-        #     key_point = (break_point['max'] - return_value) if break_point['direction'] == 0
-        #     else break_point['min'] + return_value
-        #     direction = break_point['direction']
-        #     index = break_point['index']
-        #     is_pre_enter = False
 
     key_point = '' if index == -1 else key_point
     direction = '' if index == -1 else direction
