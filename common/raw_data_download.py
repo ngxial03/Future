5e833b815b6ec22a17c21fdff4f09ccb5842f9a2
U-Edward_Wu-PC\Edward_Wu <Edward_Wu@Edward_Wu-PC.(none)>
2019-12-06 00:44:33 +0800

    update

--- common/raw_data_download.py
+++ common/raw_data_download.py
@@ -14,17 +14,13 @@ def download():
             and os.path.isfile(config.TX5_DIR + '/'+tx_dir_name+'/' + tx_file_name):
         return
 
-    print ("下載前30個交易日期貨每筆成交資料...")
     download_url = urlopen('https://www.taifex.com.tw/file/taifex/Dailydownload/DailydownloadCSV/'+file_name)
     zip_content = download_url.read()
     with open(config.ORIGINAL_DIR + '/' + file_name, 'wb') as f:
         f.write(zip_content)
 
-    print ("資料解壓縮...")
     with zipfile.ZipFile(open(config.ORIGINAL_DIR + '/' +file_name, 'rb')) as f:
         f.extractall(config.ORIGINAL_DIR)
-
-    print ("解壓縮完成!")
 
     if os.path.isfile(config.ORIGINAL_DIR + '/' + file_name):
         os.remove(config.ORIGINAL_DIR + '/' + file_name)
