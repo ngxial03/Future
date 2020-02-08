790dfe7fd7aef533b718669a53fd5abe7ac35f17
edward <ngxial03@outlook.com>
2020-02-08 23:11:54 +0800

    update

--- scheduler.py
+++ scheduler.py
@@ -3,6 +3,7 @@ from common import output, raw_data_gen, raw_data_download
 from policy import happy, pre_enter_point, max_min
 from pytz import utc
 from tzlocal import get_localzone
+import os
 import datetime
 
 scheduler = BlockingScheduler()
@@ -25,10 +26,14 @@ def job():
 
 # scheduler.add_job(job, 'interval', seconds=10)
 
-# scheduler.add_job(job, 'cron', day_of_week='mon-fri', hour=15, minute=10)
-# scheduler.add_job(job, 'cron', hour=22, minute=20)
+scheduler.add_job(job, 'cron', day_of_week='mon-fri', hour=15, minute=10)
+# scheduler.add_job(job, 'cron', hour=22, minute=29)
 
 print('start scheduler')
+os.system('git status')
+os.system('git add .')
+os.system('git commit -m "update"')
+os.system('git push')
 
 
 while True:
