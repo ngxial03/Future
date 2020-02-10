7b0b1bb16d3d72230d411ecee496c9dfc692a426
edward <ngxial03@outlook.com>
2020-02-10 23:37:04 +0800

    modify scheduler time

--- scheduler.py
+++ scheduler.py
@@ -33,7 +33,7 @@ def job():
 
 # scheduler.add_job(job, 'interval', seconds=10)
 
-scheduler.add_job(job, 'cron', day_of_week='mon-fri', hour=23, minute=35)
+scheduler.add_job(job, 'cron', day_of_week='mon-fri', hour=15, minute=15)
 # scheduler.add_job(job, 'cron', hour=22, minute=29)
 
 print('****** start scheduler ******')
