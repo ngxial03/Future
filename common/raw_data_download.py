a831876004eb6180044bb80019db4e18c25bb503
edward <ngxial03@outlook.com>
2020-06-04 15:12:43 +0800

    update

--- common/raw_data_download.py
+++ common/raw_data_download.py
@@ -7,6 +7,7 @@ import datetime
 
 
 def download():
+    print('download')
     today = datetime.date.today().strftime("%Y_%m_%d")
     file_name = 'Daily_'+today+'.zip'
     tx_dir_name = datetime.date.today().strftime("%Y%m")
@@ -23,8 +24,8 @@ def download():
     if os.path.isfile(config.TX1_DIR + '/'+tx_dir_name+'/' + tx_file_name) \
             and os.path.isfile(config.TX5_DIR + '/'+tx_dir_name+'/' + tx_file_name):
         return
-
     download_url = urlopen('https://www.taifex.com.tw/file/taifex/Dailydownload/DailydownloadCSV/'+file_name)
+    print('https://www.taifex.com.tw/file/taifex/Dailydownload/DailydownloadCSV/'+file_name)
     zip_content = download_url.read()
     with open(config.ORIGINAL_DIR + '/' + file_name, 'wb') as f:
         f.write(zip_content)
