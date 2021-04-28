d3a12f739832aaef3415f8ed8df0018f62fad063
edward <ngxial03@outlook.com>
2021-04-28 17:08:20 +0800

    add k chart

---
+++ helper/k.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import chart_studio.plotly as py
+import plotly.offline as pyoff
+import plotly.graph_objs as go
+from plotly.figure_factory import np
+
+
+def draw(date):
+    d = date[:6]
+    print(dir)
+    df = pd.read_csv('tx5_data/' + d + '/' + date + '.txt')
+    df = df.iloc[::-1]
+    fig = go.Figure(data=[go.Candlestick(x=df.Time,
+                                         open=df.Open,
+                                         high=df.High,
+                                         low=df.Low,
+                                         close=df.Close,
+                                         name=date,
+                                         increasing_line_color='red', decreasing_line_color='green')])
+    # fig.show()
+    pyoff.plot(fig)
